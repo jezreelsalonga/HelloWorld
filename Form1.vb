90fc4393d2a3373fa47fe9ef99002f0823070dba
jezreelsalonga <jezreelsalonga@gmail.com>
2015-12-15 23:40:52 +0800

    Update

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class HelloWorld
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        txtOutput.Text = "   HELLO WORLD!"
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
